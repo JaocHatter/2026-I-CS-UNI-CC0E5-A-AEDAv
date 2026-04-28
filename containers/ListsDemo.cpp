e1daaae2b48f68f872e0498b30e919204a010015
JaocHatter <jarex1012@gmail.com>
2026-04-27 19:59:29 -0500

    feat: Demos for LL (fixed), DLL, CLL, CDLL

--- containers/ListsDemo.cpp
+++ containers/ListsDemo.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <thread>
+#include <sstream>
 
 #include "../types.h"
 #include "linkedlist.h"
-// #include "doublelinkedlist.h"
-// #include "circularlinkedlist.h"
-// #include "circularlinkedlist.h"
+#include "doublelinkedlist.h"
+#include "circularlinkedlist.h"
+#include "circulardoublelinkedlist.h"
 
 using namespace std;
 
@@ -21,7 +23,7 @@ void DemoList(Container& list, string fileName){
     // Grabar la lista en un archivo
     ofstream os(fileName);
     os << list << endl;
-    
+
     // Leer la lista desde un archivo
     ifstream is(fileName);
     is >> list;
@@ -29,32 +31,39 @@ void DemoList(Container& list, string fileName){
 }
 
 void LinkedListDemo(){
-    LinkedList<T1, AscendingLinkedListTrait<T1>> list;
+    cout << "\n--- LinkedList Ascendente ---" << endl;
+    LinkedList<AscendingLinkedListTrait<T1>> list;
     DemoList(list, "AscLL.txt");
-    LinkedList<T1, DescendingLinkedListTrait<T1>> list2;
+    cout << "\n--- LinkedList Descendente ---" << endl;
+    LinkedList<DescendingLinkedListTrait<T1>> list2;
     DemoList(list2, "DescLL.txt");
 }
 
 void DoubleLinkedListDemo(){
-    // DoubleLinkedList<T1, AscendingDLLTrait<T1>> list;
-    // DemoList(list, "AscDLL.txt");
-    // DoubleLinkedList<T1, DescendingDLLTrait<T1>> list2;
-    // DemoList(list2, "DescDLL.txt");
+    cout << "\n--- DoubleLinkedList Ascendente ---" << endl;
+    DoubleLinkedList<AscendingDLLTrait<T1>> list;
+    DemoList(list, "AscDLL.txt");
+    cout << "\n--- DoubleLinkedList Descendente ---" << endl;
+    DoubleLinkedList<DescendingDLLTrait<T1>> list2;
+    DemoList(list2, "DescDLL.txt");
 }
 
 void CircularLinkedListDemo(){
-    
+    cout << "\n--- CircleLinkedList Ascendente ---" << endl;
+    CircleLinkedList<AscendingCircleLinkedListTrait<T1>> list;
+    DemoList(list, "AscCLL.txt");
+    cout << "\n--- CircleLinkedList Descendente ---" << endl;
+    CircleLinkedList<DescendingCircleLinkedListTrait<T1>> list2;
+    DemoList(list2, "DescCLL.txt");
 }
 
-void CircularLinkedListDemo(){
-    
-}
-
-void ListsDemo(){
-    LinkedListDemo();
-    CircularLinkedListDemo();
-    DoubleLinkedListDemo();
-    CircularDoubleLinkedListDemo();
+void CircularDoubleLinkedListDemo(){
+    cout << "\n--- CircularDoubleLinkedList Ascendente ---" << endl;
+    CircularDoubleLinkedList<AscendingCDLLTrait<T1>> list;
+    DemoList(list, "AscCDLL.txt");
+    cout << "\n--- CircularDoubleLinkedList Descendente ---" << endl;
+    CircularDoubleLinkedList<DescendingCDLLTrait<T1>> list2;
+    DemoList(list2, "DescCDLL.txt");
 }
 
 void TestConcurrencia() {
@@ -84,10 +93,11 @@ void TestConcurrencia() {
         cout << "ESTADO: FALLO - Hubo corrupcion de memoria." << endl;
     }
 }
+
 void TestOperators() {
     cout << "\nTEST DE OPERADORES" << endl;
     LinkedList<AscendingLinkedListTrait<T1>> list;
-    
+
     // 1. Probamos operator>> (Lectura)
     cout << "Simulando lectura desde formato: [(10, 100), (20, 200), (30, 300)]" << endl;
     stringstream simulador_input("[(10, 100), (20, 200), (30, 300)]");
@@ -95,16 +105,20 @@ void TestOperators() {
 
     // 2. Probamos operator<< (Escritura)
     cout << "Lista luego de la lectura (operator<<): " << list << endl;
-    
+
     // 3. Probamos operator[] (Acceso seguro por indice)
     cout << "Accediendo al indice [0] (operator[]): Dato -> " << list[0] << endl;
     cout << "Accediendo al indice [2] (operator[]): Dato -> " << list[2] << endl;
-    
+
     // Probamos la excepcion del operator[] (Descomentar para probar)
     // cout << "Probando fuera de rango: " << list[5] << endl; // Lanzara la excepcion
 }
+
 void ListsDemo(){
-    TestBasicos();
+    LinkedListDemo();
+    DoubleLinkedListDemo();
+    CircularLinkedListDemo();
+    CircularDoubleLinkedListDemo();
     TestConcurrencia();
     TestOperators();
     cout << "\n=== FIN DE LAS PRUEBAS ===" << endl;
