bbbe50b5c8f738f56f29432609bb028f2911657e
JaocHatter <jarex1012@gmail.com>
2026-05-18 09:41:05 -0500

    feat: main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
-#include "containers/vector.h"
-#include "containers/linkedlist.h"
+#include "BinaryTree.h"
+
 // g++ -std=c++2b main.cpp containers/vector.cpp -o main
-void ListsDemo();
+void BinaryTreeDemo();
 int main(){
     // DemoVector();
     //DemoConcurrentVector();
-    ListsDemo();
+    //ListsDemo();
+    BinaryTreeDemo();
     return 0;
 }
