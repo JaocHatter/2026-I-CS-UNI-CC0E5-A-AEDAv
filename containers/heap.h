f4f3bf7ece7552ed62b9d35d90b31edd6f49b048
JaocHatter <jarex1012@gmail.com>
2026-05-25 11:53:02 -0500

    feat: heapifyUp and heapifyDown

--- containers/heap.h
+++ containers/heap.h
@@ -43,19 +43,51 @@ public:
     using value_type = typename Trait::value_type;
     using Comp       = typename Trait::Comp;
     using MySelf     = Heap<Trait>;
-    using Node       = HeapNode<Trait>;
+    using Node       = typename Trait::Node;
     
 private:
-    Vector<Trait> m_vec;
-    Comp          m_comp;
+    Node*                m_data;
+    size_t               m_size;
+    size_t               m_capacity;
+    Comp                 m_comp;
     mutable shared_mutex m_mtx;
+
+    void resize() {
+        m_capacity = (m_capacity < 10) ? m_capacity + 10 : m_capacity * 2;
+        Node* fresh = new Node[m_capacity];
+        for (size_t i = 0; i < m_size; ++i)
+            fresh[i] = m_data[i];
+        delete[] m_data;
+        m_data = fresh;
+    }
+
+    void heapifyUp(size_t index) {
+        while (index > 0) {
+            size_t parent = (index - 1) / 2;
+            if (m_comp(m_data[index].getData(), m_data[parent].getData())) {
+                swap(m_data[index], m_data[parent]);
+                index = parent;
+            } else break;
+        }
+    }
+
+    void heapifyDown(size_t index) {
+        while (true) {
+            size_t best  = index;
+            size_t left  = 2 * index + 1;
+            size_t right = 2 * index + 2;
+            if (left  < m_size && m_comp(m_data[left].getData(),  m_data[best].getData())) best = left;
+            if (right < m_size && m_comp(m_data[right].getData(), m_data[best].getData())) best = right;
+            if (best == index) break;
+            swap(m_data[index], m_data[best]);
+            index = best;
+        }
+    }
+
 public:
     Heap() : m_vec(), m_comp() {}
     ~Heap() {}
-
-    void heapifyUp(size_t index);
-    void heapifyDown(size_t index);
-
+    
     void insert(value_type value, Ref ref);
     
     // Extrae el elemento de mayor o menor prioridad (depende del heap)
