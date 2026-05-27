76a5ffc0d52b94a9387a7be8dd94c2a38635add5
JaocHatter <jarex1012@gmail.com>
2026-05-26 20:37:56 -0500

    feat: using template type

--- containers/HeapDemo.cpp
+++ containers/HeapDemo.cpp
@@ -4,14 +4,15 @@
 #include <vector>
 #include <mutex>
 #include "heap.h"
+#include "../types.h"
 
 using namespace std;
 
 void HeapDemo() {
     cout << "\n=== MinHeap Demo ===" << endl;
 
-    Heap<MinHeapTrait<int>> minH;
-    for (int v : {5, 3, 7, 1, 4, 6, 8, 2}) minH.insert(v);
+    Heap<MinHeapTrait<Type>> minH;
+    for (Type v : {5, 3, 7, 1, 4, 6, 8, 2}) minH.insert(v);
 
     cout << "MinHeap (nivel): " << minH << endl;
     cout << "peek (min):      " << minH.peek().getData() << endl;
@@ -30,8 +31,8 @@ void HeapDemo() {
 
     cout << "\n=== MaxHeap Demo ===" << endl;
 
-    Heap<MaxHeapTrait<int>> maxH;
-    for (int v : {5, 3, 7, 1, 4, 6, 8, 2}) maxH.insert(v);
+    Heap<MaxHeapTrait<Type>> maxH;
+    for (Type v : {5, 3, 7, 1, 4, 6, 8, 2}) maxH.insert(v);
 
     cout << "MaxHeap (nivel): " << maxH << endl;
     cout << "peek (max):      " << maxH.peek().getData() << endl;
@@ -45,15 +46,15 @@ void HeapDemo() {
 
     cout << "\n=== I/O Demo ===" << endl;
 
-    Heap<MinHeapTrait<int>> h1;
-    for (int v : {9, 4, 2, 7, 5}) h1.insert(v, v * 10);
+    Heap<MinHeapTrait<Type>> h1;
+    for (Type v : {9, 4, 2, 7, 5}) h1.insert(v, v * 10);
 
     ofstream fout("MinHeap.txt");
     fout << h1;
     fout.close();
     cout << "Guardado: " << h1 << endl;
 
-    Heap<MinHeapTrait<int>> h2;
+    Heap<MinHeapTrait<Type>> h2;
     ifstream fin("MinHeap.txt");
     fin >> h2;
     fin.close();
@@ -62,25 +63,25 @@ void HeapDemo() {
 
     cout << "\n=== Copy/Move Demo ===" << endl;
 
-    Heap<MinHeapTrait<int>> h3;
-    for (int v : {10, 3, 6}) h3.insert(v);
-    Heap<MinHeapTrait<int>> h4(h3);
+    Heap<MinHeapTrait<Type>> h3;
+    for (Type v : {10, 3, 6}) h3.insert(v);
+    Heap<MinHeapTrait<Type>> h4(h3);
     cout << "Original: " << h3 << endl;
     cout << "Copia:    " << h4 << endl;
 
-    Heap<MinHeapTrait<int>> h5(move(h3));
+    Heap<MinHeapTrait<Type>> h5(move(h3));
     cout << "Movido:   " << h5 << endl;
 
     cout << "\n=== Concurrency test (5 threads x 1000 inserts) ===" << endl;
 
-    Heap<MinHeapTrait<int>> cH;
+    Heap<MinHeapTrait<Type>> cH;
     mutex printMtx;
-    auto worker = [&cH](int start) {
-        for (int i = start; i < start + 1000; ++i)
+    auto worker = [&cH](Type start) {
+        for (Type i = start; i < start + 1000; ++i)
             cH.insert(i);
     };
     vector<thread> threads;
-    for (int i = 0; i < 5; ++i) threads.emplace_back(worker, i * 1000);
+    for (T1 i = 0; i < 5; ++i) threads.emplace_back(worker, i * 1000);
     for (auto& t : threads) t.join();
     cout << "size=" << cH.size() << " (expected 5000) -> "
          << (cH.size() == 5000 ? "EXITO" : "FALLO") << endl;
